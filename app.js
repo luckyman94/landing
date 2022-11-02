13f200d9f06803337d10c1e9ce5933c2469f1733
Ilan bacry <ilanbacry@Ilans-MacBook-Pro-2.local>
2022-11-02 21:32:56 +0100

    Start

---
+++ app.js
@@ -0,0 +1,8 @@
+let navigation = document.querySelector('.navigation');
+let menuToggle = document.querySelector('.menuToggle');
+let header = document.querySelector('header');
+
+menuToggle.onclick = function(){
+    header.classList.toggle('open');
+
+}
